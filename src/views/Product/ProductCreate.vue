6eb73e687d646ecb4be2f4cf07728883f78a1220
Bekzod1010001 <itprogressuz@gmail.com>
2023-06-16 13:13:56 +0500

    add more interactivity after product creation

--- src/views/Product/ProductCreate.vue
+++ src/views/Product/ProductCreate.vue
@@ -3,8 +3,18 @@ import AppSectionTitle from "@/components/UI/AppSectionTitle/AppSectionTitle.vue
 import ProductForm from "@/components/workspace/ProductForm/ProductForm.vue";
 import ProductRepository from "@/repositories/modules/products.repository"
 import AppBreadcrumbs from "@/components/UI/AppBreadcrumbs/AppBreadcrumbs.vue";
+import {useRouter} from "vue-router";
+import useToast from "@/components/UI/AppToast/useToast";
+const router = useRouter();
+const  { toast } = useToast();
 async function createProduct(data: object) {
-  return await ProductRepository.create(data);
+  return await ProductRepository.create(data)
+      .then(() => {
+        router.push({
+          name: 'ProductIndex'
+        });
+        toast.success('Added new item');
+      });
 }
 const links = [
   {
